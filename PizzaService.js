2155c916b7e1913bc2cbc4cba6521c5155d7073b
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2021-10-10 19:38:43 +0200

    Buttons are functioning as they should

--- PizzaService.js
+++ PizzaService.js
@@ -1,12 +1,23 @@
 module.exports = function(pizzaList) {
 
 	const pizzas = pizzaList || [];
+	let cost = 0;
 
 	function listAll() {
 
 	}
 
 	function addPizza(pizza) {
+		if (pizzaType==="mediumPizza"){
+			cost+=65  
+		}
+		else if (pizzaType==="largePizza"){
+			cost+=85
+		}
+		else if (pizzaType==="smallPizza"){
+			cost+=35
+		}
+		
 
 	}
 
--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const exphbs  = require('express-handlebars');
-// const bodyParser=require("body-parser"); 
-
-// const app = express();
-
-
-// app.engine('handlebars', exphbs());
-// app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(express.static('public'));
-
-// add your routes here...
-
 const express=require("express");
 const exphbs = require("express-handlebars");
 const bodyParser=require("body-parser"); 
@@ -35,9 +19,59 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-
 app.get("/pizzas", function(req, res){
 	res.render("index")
+})
+
+app.get("/pizza/add", function(req,res){
+	res.render("add-pizza")
+
+})
+let cost=0
+app.post("/pizza/add/", function(req,res){
+    let pizzaType=req.body.size;
+    
+    if (pizzaType==="medium"){
+        cost+=65  
+    }
+    else if (pizzaType==="large"){
+        cost+=85
+    }
+    else if (pizzaType==="small"){
+        cost+=35
+    }
+    else if (pizzaType==="smallSub"){
+        cost-=35
+    }
+    else if (pizzaType==="mediumSub"){
+        cost-=65
+    }
+    else if (pizzaType==="largeSub"){
+        cost-=85
+    }
+    console.log(cost)
+
+
+    console.log(pizzaType)
+
+	res.redirect("/pizzas")
+	
+})
+
+app.get("/pizza/edit/:pizza_id", function(req,res){
+    res.render("pizza/edit")
+	let idPizza=req.params.pizza_id;
+
+    res.send(idPizza)
+
+	
+})
+
+app.post("/pizza/edit", function(req,res){
+	let idPizza=req.params.pizza_id;
+    res.send(idPizza)
+	res.redirect("/pizzas")
+	
 })
 
 
